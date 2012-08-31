1e5bcc6dd5f0f7de22ab63b51737e7d11829c398
Anirudh Joshi <github@anirudhjoshi.com>
2012-08-31 22:16:07 +1000

    fixed inline js

--- utils/build.sh
+++ utils/build.sh
@@ -6,4 +6,6 @@ java -jar compiler.jar --js ../src/js/fluid.js ../src/js/pong.js ../src/js/main.
 
 java -jar yuicompressor-2.4.7.jar ../src/css/main.css -o ../build/f.css
 
-java -jar htmlcompressor-1.5.3.jar ../src/index.html > ../build/index.html
+java -jar htmlcompressor-1.5.3.jar --compress-js --js-compressor closure ../src/index.html > ../build/index.html
+
+# java -jar htmlcompressor-1.5.3.jar ../src/index.html > ../build/index.html
