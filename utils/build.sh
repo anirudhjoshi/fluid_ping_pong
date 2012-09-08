7d631b4b94822c94ae31cae571951d5f6eea5c7f
Anirudh Joshi <github@anirudhjoshi.com>
2012-09-08 14:32:51 +1000

    updated

--- utils/build.sh
+++ utils/build.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ../build
 
-java -jar compiler.jar --js ../src/js/fluid.js ../src/js/ping_pong.js ../src/js/main.js --js_output_file ../build/f.js
+java -jar compiler.jar --js ../src/js/fluid.js ../src/js/table_tennis.js ../src/js/main.js --js_output_file ../build/f.js
 
 java -jar yuicompressor-2.4.7.jar ../src/css/main.css -o ../build/f.css
 
